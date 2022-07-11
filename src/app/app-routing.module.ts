aa9306fde252e8efdeb85aa8c348b1253479d5df
chaz <chaz.ferguson@gmail.com>
2022-07-11 16:45:45 -0400

    add bootstrap, routing, and search component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from "./components/search/search.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/search",
+    pathMatch: "full",
+  },
+  {
+    path: "search",
+    component: SearchComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
