5bb6f7ec490582bc29fc66067a220b2d3001b886
chaz <chaz.ferguson@gmail.com>
2022-07-11 20:58:16 -0400

    update github user model

--- src/app/models/github-user.ts
+++ src/app/models/github-user.ts
@@ -2,20 +2,17 @@ export class GitHubUser {
   id: number;
   login: string;
   avatar_url: string;
-  gravatar_id: string;
   url: string;
   html_url: string;
-  followers_url: string;
-  subscriptions_url: string;
-  organizations_url: string;
-  repos_url: string;
-  recevied_events_url: string;
   score: number;
-  following_url: string;
-  gists_url: string;
-  starred_url: string;
-  events_url: string;
-  site_admin: boolean;
+  name: string;
+  company: string;
+  location: string;
+  bio: string;
+  email: string;
+  followers: number;
+  following: number;
+  created_at: Date;
 
   constructor(params?: Partial<GitHubUser>) {
     if (params) {
